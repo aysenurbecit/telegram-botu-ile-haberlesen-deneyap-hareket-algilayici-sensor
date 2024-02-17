bac3c52e40d71634571d95d2153dc20bb6a2dbff
Ayşe Nur <126473192+aysenurbecit@users.noreply.github.com>
2024-02-18 00:24:39 +0300

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,63 @@
+#include <deneyap.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include <Deneyap_HareketAlgilama.h>  
+
+// Bağlanılacak WiFi bilgileri
+const char* ssid = "SUPERONLINE_plus";
+const char* password = "Mm127734**";
+
+unsigned char Hareket; 
+int hareket;
+
+// Telegramdan oluşturulan BOT bilgileri 
+#define BOTtoken "000000000000000000000000000000000000000" // Telegram uygulaması üzerinden Botfather ile öğrendiğiniz botID yi buraya yapıştırın.
+#define CHAT_ID "0000000000" // botID ile chatID'sini öğrenip buraya yapıştırın.
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+void setup() {
+Serial.begin(115200);                               // Seri monitör başlatılması
+    if (!Hareket.begin(0x32)) {                         // begin(slaveAdress) fonksiyonu ile cihazların haberleşmesi başlatılması
+        delay(3000);
+        Serial.println("I2C bağlantısı başarısız ");    // I2C bağlantısı başarısız olursa seri monitore yazdırılması
+        while (1);
+    }
+ pinMode(Hareket, INPUT);
+
+  Serial.begin(115200);
+  
+  client.setInsecure();
+  Serial.print("İnternete bağlanıyor: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi bağlandı");
+  Serial.print("IP adresi: ");
+  Serial.println(WiFi.localIP());
+  bot.sendMessage(CHAT_ID, "Cihaz aktif.");
+}
+void loop() {
+hareket = digitalRead(pir);
+  if(hareket == HIGH){
+    digitalWrite(led, HIGH);
+    Serial.print("1");
+    bot.sendMessage(CHAT_ID, "HAREKET ALGILANDI");
+  }
+  else{
+    digitalWrite(led, LOW);
+    Serial.print("0");
+}
+Serial.println("G");
+
+}
